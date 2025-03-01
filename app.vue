6ae05bef7cbfc70f1b9e4fdc9384b383aa890ccb
Emilio Bensignor <liobensignor@gmail.com>
2025-03-01 14:26:56 -0300

    Refresh lleva a la ruta de la que venis

--- app.vue
+++ app.vue
@@ -14,25 +14,33 @@ onMounted(async () => {
 
 async function setUserSession() {
   const supabase = useSupabaseClient();
+  const route = useRoute();
   const session = await supabase.auth.getSession();
-  const userInfo = session.data.session
+  const userInfo = session.data.session;
+  const currentPath = route.fullPath;
 
   if (!session.data.session) {
-    return navigateTo(ROUTE_NAMES.LOGIN);
-  } else {
-    const { data, error } = await supabase.auth.setSession({
-      access_token: userInfo.access_token,
-      refresh_token: userInfo.refresh_token,
-      expires_in: userInfo.expires_in,
-      token_type: userInfo.token_type,
-      user: userInfo.user,
-    })
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(data);
-      navigateTo(ROUTE_NAMES.HOME);
+    if (currentPath !== ROUTE_NAMES.HOME && currentPath !== ROUTE_NAMES.LOGIN) {
+      return navigateTo(ROUTE_NAMES.LOGIN);
     }
+    return;
+  }
+
+  const { data, error } = await supabase.auth.setSession({
+    access_token: userInfo.access_token,
+    refresh_token: userInfo.refresh_token,
+    expires_in: userInfo.expires_in,
+    token_type: userInfo.token_type,
+    user: userInfo.user,
+  });
+
+  if (error) {
+    console.error(error);
+    return navigateTo(ROUTE_NAMES.LOGIN);
+  }
+
+  if (currentPath === ROUTE_NAMES.LOGIN || currentPath === ROUTE_NAMES.HOME) {
+    return navigateTo(ROUTE_NAMES.HOME);
   }
 }
 </script>
